f4414c1ec1b29196194dc4c04003269f7319d234
zaynmediaworks-source <zaynmedia.works@gmail.com>
2026-09-13 21:30:38 +0800

    Mount music and admin management in active admin layout

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -3,4 +3,7 @@ import AdminSecurityGate from './AdminSecurityGate';
 import AdminPageV2 from './AdminPageV2';
 import AdminFeedbackPanel from './AdminFeedbackPanel';
 import AdminBrandExperiencePanel from './AdminBrandExperiencePanel';
-export default function Layout(){return <AdminSecurityGate><><AdminPageV2/><AdminBrandExperiencePanel/><AdminFeedbackPanel/></></AdminSecurityGate>}
+import MusicAdmin from './MusicAdmin';
+import AddAdminPanel from './AddAdminPanel';
+export default function Layout(){return <AdminSecurityGate><><nav className="admin-card" aria-label="Pengaturan admin"><a className="btn" href="#admin-music">Kelola Musik</a></nav><div id="admin-music" className="admin-shell"><MusicAdmin/><AddAdminPanel/></div><AdminPageV2/><AdminBrandExperiencePanel/><AdminFeedbackPanel/></></AdminSecurityGate>}
+
